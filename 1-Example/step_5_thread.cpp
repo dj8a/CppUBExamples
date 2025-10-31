22e415b3c6de2f8eb0bc4f76c89fffdd9d2fd6c4
dj8a <dj8a@mailo.com>
2025-10-31 14:12:10 +0000

    Add step_5

---
+++ 1-Example/step_5_thread.cpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+#include <thread>
+#include <iostream>
+
+bool is_running = true;
+int64_t counter = 0;
+
+void some_function() {
+    while (is_running) {
+        ++counter;
+    }
+}
+
+int main()
+{
+    using namespace std::chrono_literals;
+
+    std::thread worker{some_function};
+    std::this_thread::sleep_for(2s);
+    is_running = false;
+    worker.join();
+
+    std::cout << "counter - " << counter << std::endl;
+    return 0;
+}
