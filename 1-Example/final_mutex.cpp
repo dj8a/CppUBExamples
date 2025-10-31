b3551b1a0bd2336f135c4a92316b5ebe1d023ec8
dj8a <dj8a@mailo.com>
2025-10-31 12:58:07 +0000

    lower CMAKE_CXX_STANDARD requirement to C++17

---
+++ 1-Example/final_mutex.cpp
@@ -0,0 +1,30 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+bool is_running = true;
+int64_t counter = 0;
+std::mutex mutex;
+
+void some_function() {
+    std::lock_guard lock{mutex};
+    while (is_running) {
+        ++counter;
+    }
+}
+
+int main()
+{
+    using namespace std::chrono_literals;
+
+    std::thread worker{some_function};
+    std::this_thread::sleep_for(2s);
+    {
+        std::lock_guard lock{mutex};
+        is_running = false;
+    }
+    worker.join();
+
+    std::cout << "counter - " << counter << std::endl;
+    return 0;
+}
